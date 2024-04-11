818055771648d336685db7208c691aca758d33f3
RubinIn <83633061+RubinInsert@users.noreply.github.com>
2024-04-11 13:54:55 +1000

    List cards method

--- SystemInterface.java
+++ SystemInterface.java
@@ -10,7 +10,7 @@ public class SystemInterface
     private void run()
     {
         clearScreen();
-        selectPage(displayMainScreen());
+        displayMainScreen();
         //This method should control the flow of the program
     }
     int displayMainScreen() { // Returns the number the user returns when selecting a menu item. I.e. "Create a new Smart Card" -> returns 1.
@@ -25,7 +25,9 @@ public class SystemInterface
         System.out.println("8. Total Fare Costs");
         System.out.println("=================================================================");
         System.out.print("Input Menu Number: ");
-        return input.nextInt();
+        int in = input.nextInt();
+        selectPage(in);
+        return in;
     }
     void selectPage(int pageNumber) {
         switch (pageNumber) {
@@ -42,6 +44,9 @@ public class SystemInterface
             case 4: // Delete a Journey
                 break;
             case 5: // List Smart Cards
+            System.out.println("testing");
+                listAllSmartCards();
+                displayMainScreen();
                 break;
             case 6: // List Journeys on Smart Card
                 break;
@@ -61,6 +66,7 @@ public class SystemInterface
         if(smartCard1 == null || smartCard2 == null || smartCard3 == null) { // If there is an empty card slot available 
             char type = ' ';
             double balance;
+            clearScreen();
             System.out.println("What Type of Card are you creating?");
             System.out.println("1. Child");
             System.out.println("2. Senior");
@@ -84,8 +90,10 @@ public class SystemInterface
             System.out.println("Enter the balance you wish to load onto the card: "); // TODO: Add input checks e.g. if user inputs negative number, or some bs.
             balance = input.nextDouble();
             SmartCard newTempCard = new SmartCard(currentIDNumber, type, balance);
+            System.out.println("SmartCard created under ID: " + currentIDNumber);
             currentIDNumber += 1; // add one to the global variable to make sure the next card has a new number for it's ID.
             sortSmartCards(newTempCard);
+            displayMainScreen();
             
         } else { // There is no empty card slot
             showError("You have reached max number of cards. You cannot create a new card until you delete one!");
@@ -107,6 +115,20 @@ public class SystemInterface
         } else if(smartCard3 == null) {
             smartCard3 = sc;
         }
+    }
+    void listAllSmartCards() {
+        clearScreen();
+        printSmartCard(smartCard1);
+        printSmartCard(smartCard2);
+        printSmartCard(smartCard3);
+    }
+    void printSmartCard(SmartCard sc) {
+        if(sc == null) return;
+        System.out.println("============================SMART CARD===========================");
+        System.out.println("SmartID: " + sc.getSmartCardID());
+        System.out.println("Type: " + sc.getType());
+        System.out.println("Card Balance: " + sc.getBalance());
+        System.out.println("=================================================================");
     }
     public static void main(String[] args)
     {
